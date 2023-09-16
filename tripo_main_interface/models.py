c119d6da39f48d42e4ea1b492205aa3c51a2ee80
naifu <qizi297@outlook.com>
2023-09-16 15:13:49 +0800

    feat(./tripo_main_interface/models.py): Add model "Users"

--- tripo_main_interface/models.py
+++ tripo_main_interface/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+    uid = models.AutoField(primary_key=True)
+    username = models.TextField(default='unnamed user')
+    password = models.TextField(default='')
+    email = models.TextField(default='')
+    avatar = models.ImageField(upload_to='avatars')
