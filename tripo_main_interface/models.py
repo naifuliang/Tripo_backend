cd85b4ab20f94667b4ffe75dc3700b422fb915ec
naifu <qizi297@outlook.com>
2023-09-16 16:15:58 +0800

    feat: add model "Post" and annotate items

--- tripo_main_interface/models.py
+++ tripo_main_interface/models.py
@@ -1,10 +1,23 @@
 from django.db import models
 
+import image_manager.models
+
+
 # Create your models here.
 
 class Users(models.Model):
-    uid = models.AutoField(primary_key=True)
-    username = models.TextField(default='unnamed user')
-    password = models.TextField(default='')
-    email = models.TextField(default='')
-    avatar = models.ImageField(upload_to='avatars')
+    uid = models.AutoField(primary_key=True)    # primary key, uid, unique identifier in back end
+    username = models.TextField(default='unnamed user')     # username
+    password = models.TextField(default='')     # password, ought to be encrypted in front end
+    email = models.TextField(default='')    # email, format should be checked before stored
+    avatar = models.ImageField(upload_to='avatars', default=None)   # user avatar, use supported format
+    # posts, foreign key related name
+
+class Posts(models.Model):
+    post_id = models.AutoField(primary_key=True)    # primary key
+    user = models.ForeignKey('Users', on_delete=True, related_name='posts')   # user who posts
+    title = models.TextField(default='unnamed post')    # title of the post
+    content = models.TextField(default='')  # content of the post
+    time = models.TimeField()   # post time, local time
+    location = models.TextField(default='')     # location, generated & resolved in front end
+    # images, foreign key related name
